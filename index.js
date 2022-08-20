fc6c33e017263fbcde595fd46b568998539d66b0
Aghilan Nathan <aghi11@students.cs.ubc.ca>
2022-08-19 23:55:56 -0600

    Fixed bug with port number

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || "3000";
+const port = 8080;
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const { client } = require('./config.js');
